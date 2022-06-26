a640a66749d5ec761d9b5658063837d2d649a89c
Jacob Postema <jacob.postema@deluxe.com>
2022-06-25 23:19:10 -0400

    hello world

---
+++ ruby/rails_app01/spec/requests/pages_spec.rb
@@ -0,0 +1,13 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+describe "Pages", type: :request do
+  describe "GET /home" do
+    it "returns http success" do
+      get "/pages/home"
+      expect(response).to have_http_status(:success)
+    end
+  end
+
+end
